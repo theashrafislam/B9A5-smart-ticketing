b9040303ed0113de96718bf9bcaa2f6bb2c3fbf9
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-02-19 12:34:07 +0600

    JavaScript code overall Complate

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 const seatsNumber = document.querySelectorAll('.seat-number');
 let seatCount = 8;
-let seatN = 0;
+let totalSeat = 0;
+let totalPrice = 0;
 
 for (const setNum of seatsNumber) {
     setNum.addEventListener('click', function () {
-        if (seatN < 4) {
+        if (totalSeat < 4) {
             const seat = document.getElementById('seat');
-            seatN = seatN + 1;
-            seat.innerText = seatN;
+            totalSeat = totalSeat + 1;
+            seat.innerText = totalSeat;
         }
         else {
             alert('You cannot purchase more than 4 tickets.');
@@ -18,9 +19,6 @@ for (const setNum of seatsNumber) {
         seatsLeft.innerText = seatCount;
         setNum.classList.add('bg-green-500');
 
-        const trTable = document.getElementById('tr-table');
-        const tr = document.createElement('tr');
-        
         const details = document.getElementById('details');
         const div = document.createElement('div');
         const p = document.createElement('p');
@@ -31,9 +29,69 @@ for (const setNum of seatsNumber) {
         div.appendChild(p2);
         const p3 = document.createElement('p');
         p3.innerText = 550;
+        // console.log(p3.innerText)
         div.classList.add('flex', 'justify-between')
         div.appendChild(p3);
         details.appendChild(div);
+
+        totalPrice += 550;
+        const totalPriceElement = document.getElementById('total-price');
+        totalPriceElement.innerText = totalPrice;
+        const grandTotalPriceElement = document.getElementById('grand-total');
+        grandTotalPriceElement.innerText = totalPrice;
+
+
+
+
+
     })
 }
+document.getElementById('applyBtn').addEventListener('click', function () {
+    const inputElementValue = document.getElementById('input-field').value;
+    // console.log(inputElementValue);
+    if (inputElementValue === 'NEW15') {
+        const inputAll = document.getElementById('input-all');
+        inputAll.classList.add('hidden');
 
+        const discountPrice = document.getElementById('discount-price');
+        const h3 = document.createElement('h3');
+        h3.innerText = 'Discount Price';
+        h3.classList.add('text-base', 'font-medium', 'font-inter');
+        discountPrice.appendChild(h3);
+
+        const h3t = document.createElement('h3');
+        const totalPriceElement = parseInt(document.getElementById('total-price').innerText);
+        console.log( totalPriceElement);
+        h3t.innerText = 'BDT ' + (totalPriceElement * 15 / 100);
+        h3t.classList.add('text-base', 'font-medium', 'font-inter');
+        discountPrice.appendChild(h3t);
+
+        const grandTotalPriceElement = document.getElementById('grand-total');
+        grandTotalPriceElement.innerText = totalPriceElement - (totalPriceElement * 15 / 100);
+        console.log(typeof grandTotalPriceElement);
+    }
+    else if(inputElementValue === 'Couple 20'){
+        const inputAll = document.getElementById('input-all');
+        inputAll.classList.add('hidden');
+
+        const discountPrice = document.getElementById('discount-price');
+        const h3 = document.createElement('h3');
+        h3.innerText = 'Discount Price';
+        h3.classList.add('text-base', 'font-medium', 'font-inter');
+        discountPrice.appendChild(h3);
+
+        const h3t = document.createElement('h3');
+        const totalPriceElement = parseInt(document.getElementById('total-price').innerText);
+        console.log( totalPriceElement);
+        h3t.innerText = 'BDT ' + (totalPriceElement * 20 / 100);
+        h3t.classList.add('text-base', 'font-medium', 'font-inter');
+        discountPrice.appendChild(h3t);
+
+        const grandTotalPriceElement = document.getElementById('grand-total');
+        grandTotalPriceElement.innerText = totalPriceElement - (totalPriceElement * 20 / 100);
+        console.log(typeof grandTotalPriceElement);
+    }
+    else{
+        alert('nOt working')
+    }
+})
